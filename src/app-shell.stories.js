8e26f14d5e9f268fc30d51589ff26d88e397c3cc
damooo <damodara@protonmail.com>
2023-12-30 22:12:50 +0530

    Initial commit.

---
+++ src/app-shell.stories.js
@@ -0,0 +1,43 @@
+/**
+ * @file Stories for app-shell> .
+ */
+
+import { html } from 'lit';
+import './app-shell.js';
+
+/**
+ * @typedef {import('lit').TemplateResult} TemplateResult
+ */
+
+export default {
+  title: 'AppShell',
+  component: 'app-shell',
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+/**
+ * Primary template.
+ *
+ * @param  {object}         args                  Args
+ * @param  {string}         args.header           Header text
+ * @param  {string}         args.backgroundColor  background color
+ *
+ * @return {TemplateResult}                       Story Template.
+ */
+function Template({ header, backgroundColor }) {
+  return html`
+    <app-shell
+      style="--app-shell-background-color: ${backgroundColor || 'white'}"
+      .header="${header}"
+    >
+    </app-shell>
+  `;
+}
+
+export const App = Template.bind({});
+// @ts-ignore
+App.args = {
+  header: 'My app',
+};
